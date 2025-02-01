f98965efdd762f5020100881883fb8690e4821ea
nevrozza <artemker667@gmail.com>
2025-02-02 02:23:58 +0300

    fix coil for windows testMeow

--- common/utils-compose/src/commonMain/kotlin/components/GetAvatar.kt
+++ common/utils-compose/src/commonMain/kotlin/components/GetAvatar.kt
@@ -15,7 +15,6 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.ColorFilter
 import androidx.compose.ui.graphics.FilterQuality
 import androidx.compose.ui.graphics.TileMode
 import androidx.compose.ui.layout.ContentScale
@@ -43,20 +42,22 @@ fun GetAsyncIcon(
     size: Dp = 22.dp,
     modifier: Modifier = Modifier
 ) {
-    val uri = Res.getUri("drawable/icons/${path}".replace("/", "\\"))
-
+    val uri = Res.getUri("drawable/icons/${path}")
     AsyncImage(
-        ImageRequest.Builder(LocalPlatformContext.current)
-            .memoryCachePolicy(CachePolicy.ENABLED)
-            .networkCachePolicy(CachePolicy.ENABLED)
-            .data(uri)
-            .crossfade(true)
-            .build(),
-        modifier = modifier.size(size, size),
-        contentDescription = contentDescription,
-        colorFilter = ColorFilter.tint(tint)
+        model = uri,
+        null
     )
-    println("CHECK$: $uri")
+//    AsyncImage(
+//        ImageRequest.Builder(LocalPlatformContext.current)
+//            .memoryCachePolicy(CachePolicy.ENABLED)
+//            .networkCachePolicy(CachePolicy.ENABLED)
+//            .data(uri)
+//            .crossfade(true)
+//            .build(),
+//        modifier = modifier.size(size, size),
+//        contentDescription = contentDescription,
+//        colorFilter = ColorFilter.tint(tint)
+//    )
 
 //            .components {
 //                add(SvgDecoder.Factory())
